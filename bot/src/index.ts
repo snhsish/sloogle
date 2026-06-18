76284f0381ad92c902bafb669654111fa971d88a
snehasishcodes <snehasishcodes@gmail.com>
2026-06-18 22:29:34 +0530

    Initialize the slack bot and add a stats command

---
+++ bot/src/index.ts
@@ -0,0 +1,62 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { App, LogLevel } from "@slack/bolt";
+import os from "node:os";
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN!,
+    signingSecret: process.env.SLACK_SIGNING_SECRET!,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN!,
+    logLevel: LogLevel.DEBUG
+});
+
+app.event("app_mention", async ({ event, say }) => {
+    await say({
+        text: `Hello <@${event.user}>`,
+
+    })
+});
+
+app.command("/stats", async ({ command, ack, say }) => {
+    await ack();
+
+    const startDate = Date.now();
+    const uptime = process.uptime();
+    const mem = process.memoryUsage();
+    const cpus = os.cpus();
+
+    const uptimeDays = Math.floor(uptime / 86400);
+    const uptimeHours = Math.floor((uptime % 86400) / 3600);
+    const uptimeMinutes = Math.floor((uptime % 3600) / 60);
+    const uptimeSeconds = Math.floor(uptime % 60);
+
+    await say({
+        blocks: [
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: [
+                        `*BOT STATS*`,
+                        `Response Latency: \`${Date.now() - startDate}ms\``,
+                        `Uptime: \`${uptimeDays}d ${uptimeHours}h ${uptimeMinutes}m ${uptimeSeconds}s\``,
+                        `Process Memory: \`${(mem.heapUsed / 1024 / 1024).toFixed(2)} MB\` / \`${(mem.heapTotal / 1024 / 1024).toFixed(2)} MB\``,
+                        ` `,
+                        `OS: \`${os.hostname()}\` (${os.type()} ${os.release()})`,
+                        `OS Memory: \`${(os.totalmem() / 1024 / 1024 / 1024).toFixed(1)} GB\` total, \`${(os.freemem() / 1024 / 1024 / 1024).toFixed(1)} GB\` free`,
+                        `CPU: \`${cpus[0]?.model}\` (\`${cpus.length} cores\`)`,
+                        `Load Avg: \`${os.loadavg().map(n => n.toFixed(2)).join(', ')}\``
+                    ].join("\n")
+                }
+            }
+        ]
+    });
+});
+
+(async () => {
+    await app.start(process.env.PORT || 3000);
+    console.log("BOT STARTED");
+})();
+
